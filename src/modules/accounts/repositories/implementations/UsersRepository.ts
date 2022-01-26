14313f6dbc61d809ce5b56b9c77610d591a1ae99
Leonardo Gottardi <leomgottardi@gmail.com>
2022-01-26 15:58:25 -0300

    feat: criando interface e implementacao do repositorio de usuario

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,20 @@
+import { getRepository, Repository } from "typeorm";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const user = await this.repository.save(data);
+    
+    this.repository.create(user);
+  }
+}
+
+export { UsersRepository }
