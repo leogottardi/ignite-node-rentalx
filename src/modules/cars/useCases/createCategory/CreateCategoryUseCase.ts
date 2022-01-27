9e754db2d9a911afe50f46ee47cc779c500bd352
Leonardo Gottardi <leomgottardi@gmail.com>
2022-01-27 16:35:54 -0300

    feat: adicionando autenticacao e tratamento de erro

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
 
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
@@ -17,7 +18,7 @@ class CreateCategoryUseCase {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
-      throw new Error("Category Already exists!");
+      throw new AppError("Category Already exists!");
     }
 
     this.categoriesRepository.create({ name, description });
