9038ba0a61e459cbb2b20087bcc04ce6068433a6
Leonardo Gottardi <leomgottardi@gmail.com>
2022-02-14 21:47:19 -0300

    update project

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../../../database";
+import "../../typeorm";
 
 import "../../container"
 
