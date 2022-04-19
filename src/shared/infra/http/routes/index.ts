f0f981f4183cd35fba9802a279c1aa1fedad2a5b
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-18 21:01:31 -0300

    feat: create routes to reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,17 +2,19 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalRoutes } from './rental.routes';
 import { specificationRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
 
-router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/rental', rentalRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
-router.use('/rental', rentalRoutes);
 router.use(authenticateRoutes);
+router.use(passwordRoutes);
 
 export { router };
