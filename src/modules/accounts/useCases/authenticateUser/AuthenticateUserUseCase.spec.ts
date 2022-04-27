3ba642e0a340443c721e71539a6eec2ccbd874dd
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-27 08:19:26 -0300

    feat: fix tests in authenticateUserUseCase

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,61 +1,74 @@
-import { AppError } from "../../../../errors/AppError";
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
-import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
-import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { UsersTokensRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
+import { AppError } from '../../../../errors/AppError';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let dateProvider: IDateProvider;
 let createUserUseCase: CreateUserUseCase;
-describe("Authenticate User", () => {
+describe('Authenticate User', () => {
   beforeEach(() => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
-    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
+
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dateProvider,
+    );
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
   });
 
-  it("should be able to authenticate an user", async () => {
+  it('should be able to authenticate an user', async () => {
     const user: ICreateUserDTO = {
-      driver_license: "0100",
-      email: "user@example.com",
-      password: "password",
-      name: "user name"
-    }
+      driver_license: '0100',
+      email: 'user@example.com',
+      password: 'password',
+      name: 'user name',
+    };
 
     await createUserUseCase.execute(user);
 
     const result = await authenticateUserUseCase.execute({
       email: user.email,
-      password: user.password
+      password: user.password,
     });
 
-    expect(result).toHaveProperty("token");
-  })
+    expect(result).toHaveProperty('token');
+  });
 
-  it("should not be able to authenticate an nonexistent user", () => {
+  it('should not be able to authenticate an nonexistent user', () => {
     expect(async () => {
       await authenticateUserUseCase.execute({
-        email: "email@example.com",
-        password: "password",
-      })
+        email: 'email@example.com',
+        password: 'password',
+      });
     }).rejects.toBeInstanceOf(AppError);
-  })
+  });
 
-  it("should not be able to authenticate with incorrect password", async () => {
+  it('should not be able to authenticate with incorrect password', async () => {
     expect(async () => {
       const user: ICreateUserDTO = {
-        driver_license: "0100",
-        email: "user@example.com",
-        password: "1234",
-        name: "user test error"
-      }
+        driver_license: '0100',
+        email: 'user@example.com',
+        password: '1234',
+        name: 'user test error',
+      };
 
       await createUserUseCase.execute(user);
 
       await authenticateUserUseCase.execute({
         email: user.email,
-        password: "wrong password",
-      })
+        password: 'wrong password',
+      });
     }).rejects.toBeInstanceOf(AppError);
-  })
-})
+  });
+});
