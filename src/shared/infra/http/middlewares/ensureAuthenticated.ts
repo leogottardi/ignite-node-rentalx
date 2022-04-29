300b2aaccc589ab198fd206cd87b4d6a32a0815f
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-29 07:07:46 -0300

    feat: alter ensureAuthenticated to use token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -3,7 +3,6 @@ import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositor
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { AppError } from '../../../../errors/AppError';
-import { UsersRepository } from '../../../../modules/accounts/infra/typeorm/repositories/UsersRepository';
 
 interface IPayload {
   sub: string;
@@ -16,8 +15,6 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
-  const usersTokensRepository = new UsersTokensRepository();
-
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
@@ -25,21 +22,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token,
-    ) as IPayload;
-
-    const usersRepository = new UsersRepository();
-
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token,
-    );
-
-    if (!user) {
-      throw new AppError('User does not found!', 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
