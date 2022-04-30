3c9753c329e6c81d9cb3dd3ab55ec91b3b89ba03
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-30 19:37:12 -0300

    fix: Error in save LocalStorageProvider

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -8,7 +8,7 @@ class LocalStorageProvider implements IStorageProvider {
   async save(file: string, folder: string): Promise<string> {
     await fs.promises.rename(
       resolve(upload.tmpFolder, file),
-      resolve(`${upload.tmpFolder}/${folder}`),
+      resolve(`${upload.tmpFolder}/${folder}/${file}`),
     );
 
     return file;
