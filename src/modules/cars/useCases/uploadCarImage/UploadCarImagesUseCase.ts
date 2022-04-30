8ec3d29109434d52599b83385282322ce0685972
Leonardo Gottardi <leomgottardi@gmail.com>
2022-04-30 19:40:55 -0300

    feat: add upload images car

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsImageRepository } from '@modules/cars/repositories/ICarsImageRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 import { inject, injectable } from 'tsyringe';
 
 interface IRequest {
@@ -10,6 +11,8 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImageRepository')
     private carsImageRepository: ICarsImageRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async image_name => {
@@ -17,6 +20,8 @@ class UploadCarImagesUseCase {
         car_id,
         image_name,
       });
+
+      await this.storageProvider.save(image_name, 'cars');
     });
   }
 }
