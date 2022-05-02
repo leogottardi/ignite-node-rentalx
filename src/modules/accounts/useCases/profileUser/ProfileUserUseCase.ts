b28b20c6e2d0174e0cc7d0f98846ffcbe88962c8
Leonardo Gottardi <leomgottardi@gmail.com>
2022-05-02 08:56:03 -0300

    feat: create profileUserUseCase

---
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -0,0 +1,25 @@
+import { AppError } from '@errors/AppError';
+import { IUserResponseDTO } from '@modules/accounts/dtos/IUserResponseDTO';
+import { UserMap } from '@modules/accounts/mapper/UserMap';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+class ProfileUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute(id: string): Promise<IUserResponseDTO> {
+    const user = await this.usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    return UserMap.toDTO(user);
+  }
+}
+
+export { ProfileUserUseCase };
