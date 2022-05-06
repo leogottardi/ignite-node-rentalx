8b719876e894131c5e5e999a75e2ed232acb82ce
Leonardo Gottardi <leomgottardi@gmail.com>
2022-05-06 07:37:58 -0300

    feat: database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,17 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'localhost'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  if (host) {
+  return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentalx_test'
           : defaultOptions.database,
-    });
-  }
-
-  return createConnection(defaultOptions);
+    }),
+  );
 };
